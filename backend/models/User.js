ecfa1fcaa66aa6a385bd5c0f20cc346291a64191
Finnbar Home <2481383@dundee.ac.uk>
2025-03-08 18:59:37 +0000

    improved user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    password: { type: String, required: true }, // Will be hashed
-    balance: { type: Number, default: 1000 }, // Default balance
+    customerNumber: { type: String, unique: true, required: true }, // Unique Customer Number
+    password: { type: String, required: true }, // Hashed Password
+    balance: { type: Number, default: 1000 } // Default Balance
+});
+
+// Generate a unique 10-digit customer number if not provided
+UserSchema.pre("save", async function (next) {
+    if (!this.customerNumber) {
+        let uniqueNumber;
+        do {
+            uniqueNumber = Math.floor(1000000000 + Math.random() * 9000000000).toString();
+        } while (await mongoose.model("User").exists({ customerNumber: uniqueNumber }));
+        this.customerNumber = uniqueNumber;
+    }
+    next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
