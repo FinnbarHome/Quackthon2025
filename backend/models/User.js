c349de7e7a30d990fe9cd7d0787528172d625242
Finnbar Home <2481383@dundee.ac.uk>
2025-03-08 20:46:05 +0000

    No auto gen customer number

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,16 +6,4 @@ const UserSchema = new mongoose.Schema({
     balance: { type: Number, default: 1000 } // Default Balance
 });
 
-// Ensure customerNumber is generated before saving
-UserSchema.pre("validate", async function (next) {
-    if (!this.customerNumber) {
-        let uniqueNumber;
-        do {
-            uniqueNumber = Math.floor(1000000000 + Math.random() * 9000000000).toString();
-        } while (await mongoose.model("User").exists({ customerNumber: uniqueNumber }));
-        this.customerNumber = uniqueNumber;
-    }
-    next();
-});
-
 module.exports = mongoose.model("User", UserSchema);
