3d2b25ee98fb792a0d02cdd6e03ca790900b5d53
Nintails-TF <2455717@dundee.ac.uk>
2025-03-09 01:29:34 +0000

    Updating tailwind config and css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,33 @@ module.exports = {
         viga: ["Viga", "sans-serif"],
         alatsi: ["Alatsi", "sans-serif"],
       },
+      animation: {
+        "reveal": "reveal 0.7s forwards",
+        "float": "float 1s infinite ease-in-out",
+        "shimmer": "shimmer 2s infinite linear",
+      },
+      keyframes:{
+        reveal: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20px)"
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)"
+          },
+        },
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-5px)' },
+        },
+        shimmer: {
+          '0%': { backgroundPosition: '-200% 0' },
+          '100%': { backgroundPosition: '200% 0' },
+        },
+      },
     },
   },
   plugins: [],
 };
+
